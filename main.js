4d42ec51c257ece0536516f300ed7eab30c97d22
Abdulics <Abdulics93@yahoo.com>
2023-11-09 08:41:55 -0600

    Merge conflict resolved.

--- main.js
+++ main.js
@@ -27,16 +27,6 @@ const colors = [
   "chocolate",
   "coral" /* … */,
 ];
-  "aqua",
-  "azure",
-  "beige",
-  "bisque",
-  "black",
-  "blue",
-  "brown",
-  "chocolate",
-  "coral" /* … */,
-];
 
 // *** Currently a placeholder for recently said words
 const recentWords = [];
@@ -163,62 +153,6 @@ recognition.onspeechend = () => {
 recognition.onend = () => {
   document.getElementById("listening-indicator").style.display = 'none';
   listenButton.textContent = 'Record';
-};
-
-// Event handler for cases where no match is found
-recognition.onnomatch = (event) => {
-  // Display a message in the 'diagnostic' element indicating no color match
-  diagnostic.textContent = "I didn't recognize that color.";
-};
-
-// Event handler for recognition errors
-recognition.onerror = (event) => {
-  // Display an error message in the 'diagnostic' element
-  diagnostic.textContent = `Error occurred in recognition: ${event.error}`;
-};
-
-// Find and assign HTML elements by their class names
-const diagnostic = document.querySelector(".output"); // Element for displaying diagnostic messages
-const hints = document.querySelector(".hints"); // Element for displaying hints
-
-// Initialize a variable to build HTML content for color hints
-let colorHTML = "";
-
-// Loop through the 'colors' array to create HTML color hints
-colors.forEach((color, i) => {
-  console.log(color, i);
-  colorHTML += `<span style="background-color:${color};"> ${color} </span>`;
-});
-
-// Set the color hints as the content of the 'hints' element
-hints.innerHTML = `Tap or click anywhere, then say a color to change the background color of the app. Try ${colorHTML}.`;
-
-// Attach an event handler to the click event on the entire document body
-document.body.onclick = () => {
-  // Start the speech recognition process
-  recognition.start();
-  console.log("Ready to receive a color command.");
-};
-
-// Event handler for recognition results
-recognition.onresult = (event) => {
-  // Get the recognized color from the recognition results
-  const color = event.results[0][0].transcript;
-
-  // Display the recognized color in the 'diagnostic' element
-  diagnostic.textContent = `Result received: ${color}.`;
-
-  // Change the background color of the entire document to the recognized color
-  document.documentElement.style.backgroundColor = color;
-
-  // Log the confidence level of the recognition result
-  console.log(`Confidence: ${event.results[0][0].confidence}`);
-};
-
-// Event handler for the end of speech recognition
-recognition.onspeechend = () => {
-  // Stop the speech recognition process
-  recognition.stop();
 };
 
 // Event handler for cases where no match is found
