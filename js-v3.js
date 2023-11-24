39f87f50ea5e85f6670978e80ecaad93a9c99961
Abdulics <Abdulics93@yahoo.com>
2023-11-23 21:25:32 -0600

    Created a mechanism to display pronunciation diagrams for each recognized letter in a word. Developed a responsive and interactive UI that allows users to click on recognized words to view corresponding diagrams. Addressed issues related to pop-up positioning, ensuring it displays correctly above the recent word boxes. Conducted extensive testing to verify the accuracy and reliability of the word recognition and diagram display features. the htmt page is now poiting to js-v3.js for js and style-v2.css for css styling.

--- js-v3.js
+++ js-v3.js
@@ -31,6 +31,7 @@ function updateRecentWords() {
       (word) => `
         <div class="recent-word-box">
           <div class="word-title">${word}</div>
+          <div class="diagram-popup"></div>
           <button class="show-diagrams-btn">Show Diagrams</button>
         </div>
       `
@@ -62,10 +63,12 @@ recognition.onresult = (event) => {
     updateRecentWords();
 
     // Display spoken word
-    const recognizedWordElement = document.querySelector("#recognized-word span");
-    recognizedWordElement.textContent = recognizedWords
-    const recognizedWordContainer = document.querySelector('#recognized-word');
-    recognizedWordContainer.style.display = 'block';
+    const recognizedWordElement = document.querySelector(
+      "#recognized-word span"
+    );
+    recognizedWordElement.textContent = recognizedWords;
+    const recognizedWordContainer = document.querySelector("#recognized-word");
+    recognizedWordContainer.style.display = "block";
 
     // Check if the recognized word is in the predefined list
     if (predefinedWords.includes(word)) {
@@ -79,36 +82,98 @@ function handleRecognizedWord(word) {
   // Show a loading spinner or change the color of the "Record" button during recognition
   showRecognitionInProgress();
 
-  // Build the image source path
-  const imgSrc = `images\\pronunciation_images\\${word}.png`;
+  // Create an array to store image sources for each letter
+  const letterImageSources = [];
 
-  // Create an image element and set its source
-  const img = document.createElement("img");
-  img.src = imgSrc;
-
-  // Append the image to the existing diagram section
-  const diagramSection = document.querySelector(".diagram-section");
-  diagramSection.appendChild(img);
-
-  // Create a div container for the image
-  const container = document.createElement("div");
-  container.className = "image-container";
-
-  // Remove excess images beyond the last three
-  while (diagramSection.childElementCount > 3) {
-    diagramSection.removeChild(diagramSection.firstElementChild);
+  // Loop through each letter in the recognized word
+  for (const letter of word) {
+    // Build the image source path for the letter
+    const letterImgSrc = `images\\pronunciation_images\\${word}\\${letter}.png`;
+    letterImageSources.push(letterImgSrc);
   }
 
-  // Append the image to the container
-  container.appendChild(img);
-
-  // Append the container to the section
-  diagramSection.appendChild(container);
+  // Display the diagrams for each letter in the recognized word
+  displayLetterDiagrams(letterImageSources);
 
   // Reset the "Record" button after recognition
   hideRecognitionInProgress();
+}
 
-  console.log(`Image source: ${imgSrc}`);
+// Function to display diagrams for each letter in the recognized word
+function displayLetterDiagrams(letterImageSources) {
+  // Clear the existing content in the diagram section
+  const diagramSection = document.querySelector(".diagram-section");
+  diagramSection.innerHTML = "";
+
+  // Loop through each letter's image source and create and append image elements
+  letterImageSources.forEach((letterImgSrc) => {
+    const img = document.createElement("img");
+    img.src = letterImgSrc;
+
+    const container = document.createElement("div");
+    container.className = "image-container";
+    container.appendChild(img);
+
+    diagramSection.appendChild(container);
+  });
+}
+
+// Event handler for the click event on the show-diagrams-btn button
+document.addEventListener("click", (event) => {
+  console.log("Clicked element:", event.target);
+  // Check if the clicked element is a show-diagrams-btn button
+  if (event.target.classList.contains("show-diagrams-btn")) {
+    // Find the corresponding recent-word-box element
+    const recentWordBox = event.target.closest(".recent-word-box");
+
+    // Get the word associated with the recent-word-box
+    const word = recentWordBox.querySelector(".word-title").textContent;
+
+    // Get the diagrams for each letter in the word
+    const letterImageSources = [];
+    for (const letter of word) {
+      const letterImgSrc = `images\\pronunciation_images\\${word}\\${letter}.png`;
+      letterImageSources.push(letterImgSrc);
+    }
+
+    // Display a popup or modal with the diagrams
+    displayDiagramsPopup(letterImageSources);
+  }
+});
+
+// Function to display diagrams in a popup or modal
+function displayDiagramsPopup(letterImageSources) {
+  console.log("Displaying popup with sources:", letterImageSources);
+  // Remove any existing popups
+  const existingPopup = document.querySelector(".diagram-popup");
+  if (existingPopup) {
+    existingPopup.parentElement.removeChild(existingPopup);
+  }
+  // Create a popup or modal element
+  const popup = document.createElement("div");
+  popup.className = "diagram-popup";
+
+  // Close button
+  const closeBtn = document.createElement("button");
+  closeBtn.className = "close-popup-btn";
+  closeBtn.innerHTML = "&times;"; // Close symbol (X)
+  closeBtn.addEventListener("click", () => {
+    document.querySelector(".recent-section").removeChild(popup);//body.removeChild(popup);
+  });
+  popup.appendChild(closeBtn);
+
+  // Loop through each letter's image source and create and append image elements
+  letterImageSources.forEach((letterImgSrc) => {
+    const img = document.createElement("img");
+    img.src = letterImgSrc;
+    popup.appendChild(img);
+  });
+
+  // Append the popup to the recent section
+  document.querySelector(".recent-section").appendChild(popup);
+
+  // Add the "open" class to make the close button visible
+  popup.classList.add("open");
 }
 
 // Function to show loading spinner or change the color of the "Record" button
